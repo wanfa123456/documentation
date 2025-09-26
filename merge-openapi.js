a778800825c3aaae7745e5ee2c1e50b24696c6a2
Wanfa Xu <743709284@qq.com>
2025-09-26 11:00:52 +0800

    fix

---
+++ merge-openapi.js
@@ -0,0 +1,18 @@
+const { exec } = require('child_process');
+
+console.log('Watching for OpenAPI file changes...');
+
+exec(
+  'openapi bundle api-reference/openapi.json  --output openapi-merged.json',
+  (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error while merging OpenAPI files: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.error(`Stderr: ${stderr}`);
+      // return;
+    }
+    console.log(`Swagger CLI Output:\n${stdout}`);
+  }
+);
